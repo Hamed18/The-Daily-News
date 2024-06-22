f717640358076958a99ff4d92ebae674d8e52551
hamed18 <hamedcnec@gmail.com>
2024-06-22 18:45:23 +0600

    Right Side Navbar: login and Find Us section

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Website Name: The Daily News
 
 # Dynamic Data Time: moment js (visit: https://momentjs.com/docs/#/displaying/)
 # Dynamic slider: breaking news slider using react fast marquee (visit: https://www.react-fast-marquee.com/documentation/#usage)
+# navbar: https://daisyui.com/components/navbar/
+# react icon: https://react-icons.github.io/react-icons/
+# 
--- src/pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,8 +1,33 @@
-
+import { FaFacebook, FaGithub, FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 const RightSideNav = () => {
 	return (
 		<div>
-			<h2 className="">right side nav</h2>
+			<div className="p-4 mb-6 border rounded-lg border-black">
+			   <h2 className="text-3xl mb-3">Login With</h2>
+			   <button className="btn btn-outline w-full mb-2">
+                   <FaGoogle></FaGoogle>
+				   Google
+			    </button>
+			   <button className="btn btn-outline w-full">
+                   <FaGithub></FaGithub>
+				   Github
+			    </button>
+			</div>
+			<div className="p-4 mb-6 border border-black rounded-lg">
+			   <h2 className="text-3xl mb-3">Find Us on</h2>
+			   <a href="" className="p-4 flex items-center text-lg border border-black rounded-t-lg">
+				   <FaFacebook className="mr-3"></FaFacebook>
+				   <span>Facebook</span> 
+			   </a>
+			   <a href="" className="p-4 flex items-center text-lg border-x border-black">
+				   <FaTwitter className="mr-2"></FaTwitter>
+				   <span>Twitter</span> 
+			   </a>
+			   <a href="" className="p-4 flex items-center text-lg border border-black rounded-b-lg">
+				   <FaInstagram className="mr-2"></FaInstagram>
+				   <span>Instagram</span> 
+			   </a>
+			</div>
 		</div>
 	);
 };
