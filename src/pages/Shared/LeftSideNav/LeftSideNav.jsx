ab6d6b8dcc1ae512c1e96eec85ad93b22a6046cc
hamed18 <hamedcnec@gmail.com>
2024-06-22 20:29:05 +0600

    Left Sidebar Navigation added

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
+	const [categories, setCategories] = useState([]); 
+
+	useEffect( () =>{
+		fetch('categories.json')
+		.then(res => res.json())
+		.then(data => setCategories(data))
+	},[])
 	return (
-		<div>
-			<h3 className="">Left Side Nav</h3>
+		<div className="space-y-6">
+			<h3 className="text-2xl">All Categories</h3>
+			{
+				categories.map(category => <Link className="block ml-4 text-xl font-semibold px-4" 
+				key={category.id}
+				to={'/category/${category.id}'}> {category.name} </Link>)
+			}
 		</div>
 	);
 };
